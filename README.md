8e64831411bfb6485419c5549d6aba9ba103c1d4
lixinyu-ai2026 <3248975015@qq.com>
2026-06-02 16:09:38 +0800

    Initial commit: Spring Boot personal blog

---
+++ README.md
@@ -0,0 +1,17 @@
+# My Blog
+
+
+Spring Boot 版本为 2.7.5
+
+
+## 注意事项
+
+- **数据库文件目录为```static-files/my_blog_db.sql```；**
+- **My Blog 后台管理系统的默认登陆账号为 admin 默认登陆密码为 L123789；**
+
+[![Build Status](https://travis-ci.org/ZHENFENG13/My-Blog.svg?branch=master)](https://travis-ci.org/ZHENFENG13/My-Blog)
+![Version 4.0.0](https://img.shields.io/badge/version-4.0.0-yellow.svg)
+[![License](https://img.shields.io/badge/license-apache-blue.svg)](https://github.com/ZHENFENG13/My-Blog/blob/master/LICENSE)
+
+## 感谢
+
---
+++ src/main/java/com/site/blog/my/core/dao/BlogLinkMapper.java
@@ -0,0 +1,27 @@
+package com.site.blog.my.core.dao;
+
+import com.site.blog.my.core.entity.BlogLink;
+import com.site.blog.my.core.util.PageQueryUtil;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface BlogLinkMapper {
+    int deleteByPrimaryKey(Integer linkId);
+
+    int insert(BlogLink record);
+
+    int insertSelective(BlogLink record);
+
+    BlogLink selectByPrimaryKey(Integer linkId);
+
+    int updateByPrimaryKeySelective(BlogLink record);
+
+    int updateByPrimaryKey(BlogLink record);
+
+    List<BlogLink> findLinkList(PageQueryUtil pageUtil);
+
+    int getTotalLinks(PageQueryUtil pageUtil);
+
+    int deleteBatch(Integer[] ids);
+}
